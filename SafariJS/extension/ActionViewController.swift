42769f06177f587942a5f4757e4011700d434db8
Kevia <cloud@kevia.me>
2016-02-01 18:02:16 -0800

    keyboard fixed project wrap up

--- SafariJS/extension/ActionViewController.swift
+++ SafariJS/extension/ActionViewController.swift
@@ -19,6 +19,8 @@ class ActionViewController: UIViewController
     {
         super.viewDidLoad()
         
+        beAnObserver()
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "done")
         
         if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem
@@ -58,6 +60,32 @@ class ActionViewController: UIViewController
         item.attachments = [customJavaScript]
         
         extensionContext!.completeRequestReturningItems([item], completionHandler: nil)
+    }
+    
+    func beAnObserver()
+    {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    func adjustForKeyboard(notification: NSNotification)
+    {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let keyboardViewEndFrame = view.convertRect(keyboardScreenEndFrame, fromView: view.window)
+        
+        if notification.name == UIKeyboardWillHideNotification {
+            script.contentInset = UIEdgeInsetsZero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        script.scrollIndicatorInsets = script.contentInset
+        
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
     }
 
 }
